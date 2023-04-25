415e5bbabff148caca9fa4f3228466aeb19894a8
Arman <arman.saqarchi@gmail.com>
2023-04-26 00:17:10 +0330

    adding procedure register

--- core/DDL/tables/account.sql
+++ core/DDL/tables/account.sql
@@ -27,6 +27,13 @@ CREATE OR REPLACE FUNCTION make_username ()
     $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION make_balance ()
+    RETURNS TRIGGER AS $$
+        BEGIN
+            INSERT INTO TABLE latest_balances(accountNumber, amount) VALUES (NEW.accountNumber, NEW.amount)
+            RETURN NEW;
+        END;
+    $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER username AFTER INSERT ON account FOR EACH ROW
     EXECUTE FUNCTION make_username();
@@ -35,4 +42,16 @@ CREATE TRIGGER username AFTER INSERT ON account FOR EACH ROW
 CREATE TRIGGER balance AFTER INSERT ON account FOR EACH ROW
     EXECUTE FUNCTION make_balance();
 
+
+CREATE OR REPLACE PROCEDURE register(accountNumber NUMERIC(16, 0), password VARCHAR(60),
+firstname VARCHAR(60), lastname VARCHAR(60), nationalID NUMERIC(10, 0), birth_of_date DATE, type STATUS, interest_rate INT)
+AS $$
+    DECLARE hashed_password VARCHAR(60);
+    BEGIN
+        IF EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birth_of_date) > 13 THEN
+            SET hashed_password encode(DIGEST(password, 'sha256'), 'hex')
+            INSERT INTO account(accountNumber, password, firstname, lastname, nationalID, birth_of_date, interest_rate)
+            VALUES(accountNumber, password, firstname. lastname, nationalID, birth_of_date, interest_rate);
+    END;
+$$ LANGUAGE plpgsql 
 
