d13cb2fe3fde4fdb8dacea9c03c4a3d1d7f8a91c
Arman <arman.saqarchi@gmail.com>
2023-05-02 22:44:41 +0330

    debugging DDLs

--- db/procedures/balance_procedure.sql
+++ db/procedures/balance_procedure.sql
@@ -1,11 +1,13 @@
-
-CREATE TRIGGER balance AFTER INSERT ON account FOR EACH ROW
-    EXECUTE FUNCTION make_new_balance();
-
 CREATE OR REPLACE FUNCTION make_new_balance()
     RETURNS TRIGGER AS $$
         BEGIN
-            INSERT INTO latest_balances(accountNumber, amount) VALUES (NEW.accountNumber, 0)
+            INSERT INTO latest_balances(accountNumber, amount) VALUES (NEW.accountNumber, 0);
             RETURN NEW;
         END;
 $$ LANGUAGE plpgsql;
+
+
+
+
+CREATE TRIGGER balance AFTER INSERT ON account FOR EACH ROW
+    EXECUTE FUNCTION make_new_balance();
